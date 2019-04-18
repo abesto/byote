255a48ed359a16e45fe31348a8c6d77107badb14
Zoltan Nagy <abesto0@gmail.com>
2019-04-18 21:30:16 +0100

    Step 4: press `q` to quit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use std::io::Read;
 
 fn main() {
     let mut c: [u8; 1] = [0];
-    while std::io::stdin().read_exact(&mut c).is_ok() {
+    while std::io::stdin().read_exact(&mut c).is_ok() && c != ['q' as u8] {
     }
 }
