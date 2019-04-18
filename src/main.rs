18dbf7f194c7c00870f2e0549580d1179070d7e3
Zoltan Nagy <abesto0@gmail.com>
2019-04-18 23:33:29 +0100

    Step 16: A timeout for `read()`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate lazy_static;
 use std::io::Read;
 use std::os::unix::io::{AsRawFd, RawFd};
 use termios::{Termios, tcsetattr, TCSAFLUSH, ECHO, ICANON, ISIG, IXON, IEXTEN, ICRNL, OPOST,
-              BRKINT, INPCK, ISTRIP, CS8};
+              BRKINT, INPCK, ISTRIP, CS8, VMIN, VTIME};
 use libc::atexit;
 
 lazy_static! {
@@ -24,24 +24,31 @@ fn enable_raw_mode() {
     let mut raw: Termios = *ORIG_TERMIOS;
     raw.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= !(OPOST);
-    raw.c_cflag |= (CS8);
+    raw.c_cflag |= CS8;
     raw.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
     tcsetattr(*STDIN, TCSAFLUSH, &mut raw).unwrap();
 }
 
 fn main() {
     enable_raw_mode();
 
-    let mut buffer: [u8; 1] = [0];
     let exit = ['q' as u8];
     let mut stdin = std::io::stdin();
-    while stdin.read_exact(&mut buffer).is_ok() && buffer != exit {
+
+    loop {
+        let mut buffer: [u8; 1] = [0];
+        stdin.read(&mut buffer).unwrap();
         let n = buffer[0];
         let c = n as char;
         if c.is_ascii_control() {
             print!("{}\r\n", n)
         } else {
             print!("{} ('{}')\r\n", n, c);
+        }
+        if buffer == exit {
+            break;
         }
     }
 }
