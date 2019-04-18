307772e0b758cc03096ccd4a6403bdc103293a0e
Zoltan Nagy <abesto0@gmail.com>
2019-04-18 21:46:55 +0100

    Step 5: disable echoing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::io::Read;
+use termios::{Termios, ECHO, tcsetattr, TCSAFLUSH};
+use std::os::unix::io::AsRawFd;
+
+fn enable_raw_mode() {
+    let fd = std::io::stdin().as_raw_fd();
+    let mut termios = Termios::from_fd(fd).unwrap();
+    termios.c_lflag &= !ECHO;
+    tcsetattr(fd, TCSAFLUSH, &mut termios).unwrap();
+}
 
 fn main() {
+    enable_raw_mode();
+
     let mut c: [u8; 1] = [0];
     while std::io::stdin().read_exact(&mut c).is_ok() && c != ['q' as u8] {
     }
