7566c71c864c5f162d1bc7dd150800fbcd906a88
Zoltan Nagy <abesto0@gmail.com>
2019-04-19 18:40:10 +0100

    Step 26: Global state

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,17 @@ fn ctrl_key(k: u8) -> u8 {
 
 /*** data ***/
 
+struct EditorConfig {
+    orig_termios: Termios,
+}
+
 lazy_static! {
     static ref STDIN_RAWFD: RawFd = std::io::stdin().as_raw_fd();
-    static ref ORIG_TERMIOS: Termios = Termios::from_fd(*STDIN_RAWFD)
-        .map_err(|_| die("ORIG_TERMIOS"))
-        .unwrap();
+    static ref E: EditorConfig = EditorConfig {
+        orig_termios: Termios::from_fd(*STDIN_RAWFD)
+            .map_err(|_| die("ORIG_TERMIOS"))
+            .unwrap()
+    };
 }
 
 /*** terminal ***/
@@ -40,7 +46,7 @@ fn die(s: &str) {
 }
 
 extern "C" fn disable_raw_mode() {
-    if tcsetattr(*STDIN_RAWFD, TCSAFLUSH, &*ORIG_TERMIOS).is_err() {
+    if tcsetattr(*STDIN_RAWFD, TCSAFLUSH, &E.orig_termios).is_err() {
         die("disable_raw_mode/tcsetattr");
     }
 }
@@ -50,7 +56,7 @@ fn enable_raw_mode() {
         atexit(disable_raw_mode);
     };
 
-    let mut raw: Termios = *ORIG_TERMIOS;
+    let mut raw: Termios = E.orig_termios;
     raw.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= !(OPOST);
     raw.c_cflag |= CS8;
